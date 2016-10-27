b27aa4be106a15255859bc0f153925bd297c8126
Stephen Donner <stephen.donner@gmail.com>
2016-10-26 22:15:48 -0700

    More bootcamp -> doc[s|umentation] renaming

--- conf.py
+++ conf.py
@@ -218,7 +218,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'fxtestengbootcamp', u'Firefox Test Engineering Bootcamp Documentation',
+    ('index', 'fxtestengdocs', u'Firefox Test Engineering Documentation',
      [u'Firefox Test Engineering'], 1)
 ]
 
